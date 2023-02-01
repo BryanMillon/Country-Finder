66f033b1e7edeb3f7d5c9878ab26b5e4a03bfff0
Bryan Millon <bryanmillon31@gmail.com>
2023-02-01 13:55:19 -0600

    add pg

--- api/src/db.js
+++ api/src/db.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
+const pg = require('pg');
 const path = require('path');
 const {
   DB_USER, DB_PASSWORD, DB_HOST, ELEPHANTDB_PASSWORD,PGHOST,PGDATABASE,PGUSER,PGPASSWORD
