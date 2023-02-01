a8b66768d847c3f0b73a5aff7a6300f78a6ab9db
Bryan Millon <bryanmillon31@gmail.com>
2023-01-31 23:18:04 -0600

    fix

--- api/index.js
+++ api/index.js
@@ -17,14 +17,13 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const{getCountries}= require ('./src/routes/index.js')
+//const{getCountries}= require ('./src/routes/index.js')
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
-    getCountries();
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
